46da6a7c044225a442a40273c15b4592c9318af3
corentineuvrard <corentin.euvrard@gmail.com>
2021-12-14 15:01:45 +0900

    Solve day 6

--- 2021/Day06/day06.py
+++ 2021/Day06/day06.py
@@ -1,41 +1,47 @@
 from typing import List
+from collections import Counter
 
 
 def parse_input(input_file: str) -> List[int]:
     """
     Parse input.
     @param input_file: Input file to parse.
-    @return: List of endpoints representing vent segments.
+    @return: List representing the number of fishes for each creation timer (from 0 to 8 days).
     """
     with open(input_file, "r") as f:
-        lines = f.readlines()
-    return [int(n) for n in lines[0].split(",")]
+        creation_timers = Counter(int(timer) for timer in f.readlines()[0].split(","))
+    return [creation_timers[i] if i in creation_timers else 0 for i in range(9)]
 
 
-def part1(fish: List[int]) -> int:
+def count_fishes(creation_timers: List[int], days: int) -> int:
+    """
+    Count the number of fishes for a given number of days.
+    @param creation_timers: List representing the number of fishes for each creation timer (from 0 to 8 days).
+    @param days:
+    @return:
+    """
+    timers = creation_timers[:]
+    for day in range(days):
+        timers[0:6], timers[6], timers[7], timers[8] = timers[1:7], timers[0] + timers[7], timers[8], timers[0]
+    return sum(timers)
+
+
+def part1(creation_timers: List[int]) -> int:
     """
     Solve part 1.
-    @param fish: List of fish with each value representing the number of days until the fish creates a new fish.
+    @param creation_timers: List representing the number of fishes for each creation timer (from 0 to 8 days).
     @return: Number of fish after 80 days.
     """
-    days = 80
-    nb_fish = len(fish)
-    memo = {}
+    return count_fishes(creation_timers, 80)
 
-    i = 0
-    while i < len(fish):
-        if fish[i] not in memo:
-            creation_days = []
-            j = 0
-            while fish[i] + 1 + j * 7 <= days:
-                creation_days.append(fish[i] + 1 + j * 7)
-                j += 1
-            memo[fish[i]] = creation_days
-        for creation_day in memo[fish[i]]:
-            fish.append(creation_day + 8)
-            nb_fish += 1
-        i += 1
-    return nb_fish
+
+def part2(creation_timers: List[int]) -> int:
+    """
+    Solve part 2.
+    @param creation_timers: List representing the number of fishes for each creation timer (from 0 to 8 days).
+    @return: Number of fish after 256 days.
+    """
+    return count_fishes(creation_timers, 256)
 
 
 def solve() -> None:
@@ -44,6 +50,7 @@ def solve() -> None:
     """
     puzzle_input = parse_input("input.txt")
     print(part1(puzzle_input))
+    print(part2(puzzle_input))
 
 
 solve()
