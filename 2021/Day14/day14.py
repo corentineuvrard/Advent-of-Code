d36daf63d92b63d6990003b332a144a083bd8733
corentineuvrard <corentin.euvrard@gmail.com>
2022-03-15 12:09:48 +0900

    Update day 14

--- 2021/Day14/day14.py
+++ 2021/Day14/day14.py
@@ -21,15 +21,17 @@ def parse_input(input_file: str) -> Tuple[str, Dict[str, str]]:
     return polymer_template, pair_insertion
 
 
-def part1(manual: Tuple[str, Dict[str, str]]) -> int:
+def polymerize(manual: Tuple[str, Dict[str, str]], steps: int) -> int:
     """
-    Solve part 1.
+    Apply a number of steps of pair insertion to the polymer template contained in the manual.
     @param manual: Manual containing a polymer template and a list of pair insertion rules.
-    @return: Subtraction of the quantity of the most common element and the quantity of the least common element.
+    @param steps: Number of pair insertion to apply to the polymer template.
+    @return: Subtraction of the quantity of the most common element and the quantity of the least common element in the
+    final polymer.
     """
     polymer, pairs = manual
-    pairs_counter = Counter([polymer[i:i+2] for i in range(len(polymer) - 1)])
-    for _ in range(10):
+    pairs_counter = Counter([polymer[i:i + 2] for i in range(len(polymer) - 1)])
+    for _ in range(steps):
         temp_counter = pairs_counter.copy()
         for pair in [key for key in pairs_counter.keys() if pairs_counter[key] > 0]:
             pairs_counter[pair] -= temp_counter[pair]
@@ -39,26 +41,24 @@ def part1(manual: Tuple[str, Dict[str, str]]) -> int:
     for pair in pairs_counter.keys():
         elements[pair[1]] += pairs_counter[pair]
     return max(elements.values()) - min(elements.values())
+
+
+def part1(manual: Tuple[str, Dict[str, str]]) -> int:
+    """
+    Solve part 1.
+    @param manual: Manual containing a polymer template and a list of pair insertion rules.
+    @return: Result of the polymerization after applying 10 steps of pair insertion.
+    """
+    return polymerize(manual, 10)
 
 
 def part2(manual: Tuple[str, Dict[str, str]]) -> int:
     """
     Solve part 2.
     @param manual: Manual containing a polymer template and a list of pair insertion rules.
-    @return: Subtraction of the quantity of the most common element and the quantity of the least common element.
+    @return: Result of the polymerization after applying 40 steps of pair insertion.
     """
-    polymer, pairs = manual
-    pairs_counter = Counter([polymer[i:i + 2] for i in range(len(polymer) - 1)])
-    for _ in range(40):
-        temp_counter = pairs_counter.copy()
-        for pair in [key for key in pairs_counter.keys() if pairs_counter[key] > 0]:
-            pairs_counter[pair] -= temp_counter[pair]
-            pairs_counter[pair[0] + pairs[pair]] += temp_counter[pair]
-            pairs_counter[pairs[pair] + pair[1]] += temp_counter[pair]
-    elements = Counter(polymer[0])
-    for pair in pairs_counter.keys():
-        elements[pair[1]] += pairs_counter[pair]
-    return max(elements.values()) - min(elements.values())
+    return polymerize(manual, 40)
 
 
 def solve() -> None:
