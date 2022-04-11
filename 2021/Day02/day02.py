6d701a820c606fa69937f8bbe0c9c244d1748d7e
corentineuvrard <corentin.euvrard@gmail.com>
2022-04-11 23:48:06 +0900

    Update day 2

--- 2021/Day02/day02.py
+++ 2021/Day02/day02.py
@@ -7,7 +7,7 @@ def parse_input(input_file: str) -> List[List[Union[str, int]]]:
     @param input_file: Input file to parse.
     @return: List of commands with their name and units.
     """
-    with open(input_file, "r") as f:
+    with open(input_file, 'r') as f:
         lines = f.readlines()
     return [[line.split()[0], int(line.split()[1])] for line in lines]
 
@@ -21,11 +21,11 @@ def part1(commands: List[List[Union[str, int]]]) -> int:
     horizontal_position, depth = 0, 0
     for command in commands:
         name, units = command
-        if name == "forward":
+        if name == 'forward':
             horizontal_position += units
-        elif name == "down":
+        elif name == 'down':
             depth += units
-        elif name == "up":
+        elif name == 'up':
             depth -= units
     return horizontal_position * depth
 
@@ -39,12 +39,12 @@ def part2(commands: List[List[Union[str, int]]]) -> int:
     horizontal_position, depth, aim = 0, 0, 0
     for command in commands:
         name, units = command
-        if name == "forward":
+        if name == 'forward':
             horizontal_position += units
             depth += units * aim
-        elif name == "down":
+        elif name == 'down':
             aim += units
-        elif name == "up":
+        elif name == 'up':
             aim -= units
     return horizontal_position * depth
 
@@ -53,7 +53,7 @@ def solve() -> None:
     """
     Solve the puzzle.
     """
-    puzzle_input = parse_input("input.txt")
+    puzzle_input = parse_input('input.txt')
     print(part1(puzzle_input))
     print(part2(puzzle_input))
 
