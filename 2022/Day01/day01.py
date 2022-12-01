f016717acb86caf43125d472a5eaf0dbe60d826c
corentineuvrard <corentin.euvrard@gmail.com>
2022-12-02 01:20:14 +1000

    Solve day 1

---
+++ 2022/Day01/day01.py
@@ -0,0 +1,52 @@
+from typing import List
+
+
+def parse_input(input_file: str) -> List[List[int]]:
+    """
+    Parse input.
+    @param input_file: Input file to parse.
+    @return: List representing the calories of the food carried by the elves.
+    """
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
+        calories = []
+        elf_calories = []
+        for line in lines:
+            if line == '\n':
+                calories.append(elf_calories)
+                elf_calories = []
+            else:
+                elf_calories.append(int(line[:-1]))
+    return calories
+
+
+def part1(calories: List[List[int]]) -> int:
+    """
+    Solve part 1.
+    @param calories: List representing the calories of the food carried by the elves.
+    @return: Total calories carried by the elf carrying the most calories.
+    """
+    return max(list(map(sum, calories)))
+
+
+def part2(calories: List[List[int]]) -> int:
+    """
+    Solve part 2.
+    @param calories: List representing the calories of the food carried by the elves.
+    @return: Total calories carried by the three elves carrying the most calories.
+    """
+    total_calories = list(map(sum, calories))
+    total_calories.sort(reverse=True)
+    return sum(total_calories[:3])
+
+
+def solve() -> None:
+    """
+    Solve the puzzle
+    """
+    puzzle_input = parse_input('input.txt')
+    print(part1(puzzle_input))
+    print(part2(puzzle_input))
+
+
+solve()
